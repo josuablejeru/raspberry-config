476fb499573fe3c1789146eaa238aa15bbbddbfa
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-09-18 15:21:24 +0200

    add grafana as a dependencie

--- dependencies/install.bash
+++ dependencies/install.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "install packages from packages.txt"
 
+# Add grafana package key and the grafana repository
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
 xargs -r sudo apt-get install -y <packages.txt
